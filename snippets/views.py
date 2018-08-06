5f11951d7f7465735a8f7dfbdc3f294b4f95ae24
uni-lovenix <loveyanga@qq.com>
2018-08-06 22:59:55 +0800

    Using ModelSerializer.

--- snippets/views.py
+++ snippets/views.py
@@ -1,3 +1,45 @@
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from rest_framework.renderers import JSONRenderer
+from rest_framework.parsers import JSONParser
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer
 
-# Create your views here.
+@csrf_exempt
+def snippet_list(request):
+	if request.method == 'GET':
+		snippets = Snippet.objects.all()
+		serializers = SnippetSerializer(snippets, many=True)
+		return JsonResponse(serializers.data, safe=False)
+
+	if request.method == 'POST':
+		data = JSONParser().parse(request)
+		serializer = SnippetSerializer(data=data)
+		if serializer.is_valid():
+			serializer.save()
+			return JsonResponse(serializer.data, status=201)
+		return JsonResponse(serializer.error, status=400)
+
+@csrf_exempt
+def snippet_detail(request, pk):
+	try:
+		snippet = Snippet.objects.get(pk=pk)
+	except DoseNotExist:
+		return HttpResponse(status=404)
+
+	if request.method == 'GET':
+		serializer = SnippetSerializer(snippet)
+		return JsonResponse(serializer.data)
+
+	elif request.method == 'PUT':
+		data = JSONParser().parse(request)
+		serializer = SnippetSerializer(snippet, data=data)
+		if serializer.is_valid():
+			serializer.save()
+			return JsonResponse(serializer.data)
+		return JsonResponse(serializer.error, status=400)
+
+	elif request.method == 'DELETE':
+		snippet.delete()
+		return HttpResponse(status=204)
